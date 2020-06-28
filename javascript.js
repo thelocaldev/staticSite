97a6484aa8a9920e596aa9f211c1839596358522
Utkarsh Srivastav <utkarsh.srivastav@symbtechnologies.com>
2020-06-28 18:33:35 +0530

    initial commit

---
+++ javascript.js
@@ -0,0 +1,8 @@
+
+            function wordBank(){
+                for(i=1;i<99;i++){
+                    console.log(`${i} Hello world`)
+
+                }
+            }
+            wordBank()
